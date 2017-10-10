5e33ffcbbbd0ad754c2880b8a823d8b05f4a2ba4
I <mac@IdeMacBook-Pro.local>
2017-10-10 20:40:35 +0800

    Initial Commit

---
+++ LOLHero/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOLHero
+//
+//  Created by LongCh on 2017/10/10.
+//  Copyright © 2017年 LongCh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LOLHero/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LOLHero
+//
+//  Created by LongCh on 2017/10/10.
+//  Copyright © 2017年 LongCh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
